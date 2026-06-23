3c67bbc45eb477ced0128d2ca48c57faa063fe5e
drkntczxn <ntr7nicolas@gmail.com>
2026-06-23 12:12:04 -0300

    edit working.

--- update.php
+++ update.php
@@ -1,17 +1,17 @@
 <?php
 include "connection.php";
 
-$id = $_POST['id_cust'];
-$name = $_POST['name_cust'];
-$email = $_POST['email_cust'];
+$id = $_POST['id'];
+$name = $_POST['name'];
+$email = $_POST['email'];
 $wash_type = $_POST['wash_type'];
 $wax = $_POST['wax'];
 
 $sql = "UPDATE customer SET
-name_cust to '$name',
-email_cust to '$email',
-wash_type to '$wash_type',
-wax to '$wax'
+name_cust = '$name',
+email_cust = '$email',
+wash_type = '$wash_type',
+wax = '$wax'
 WHERE id_cust = $id";
 
 mysqli_query($con, $sql);
